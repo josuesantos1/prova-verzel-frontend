ee52ce5e299af5b98b6dd1791997d74360f78632
josuesantos1 <josuecsantos2002@gmail.com>
2022-03-09 16:42:44 -0300

    :art: (form) Search component

---
+++ components/form.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Search = styled.input`
+    border: 1px solid #ccc;
+    border-radius: 0.5rem;
+
+    width: 50vw;
+    padding: 1rem;
+
+    color: #76f;
+
+    &:focus {
+        outline: none;
+        font-weight: bold;
+    }
+`;
+
