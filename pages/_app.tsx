050055ed6a50fff1e7d8930c3fea41613ba9240b
josuesantos1 <josuecsantos2002@gmail.com>
2022-03-09 15:59:30 -0300

    :art: use App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { App } from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <App>
+    <Component {...pageProps} />
+  </App>
 }
 export default MyApp
