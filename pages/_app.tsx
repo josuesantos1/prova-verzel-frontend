f0bb811dcbbc07fcb0341c219a7f38507bd170eb
josuesantos1 <josuecsantos2002@gmail.com>
2022-03-13 14:31:06 -0300

    :book: (root) NavBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app'
-import { App } from '../components/layout'
+import Link from 'next/link'
+import { App, NavBar } from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <App>
+    <NavBar>
+      <Link href="/">Home</Link>
+      <ul>
+        <li>
+          <Link href="/signin">Sign in</Link>
+        </li>
+        <li>
+          <Link href="/signup">Sign up</Link>
+        </li>
+      </ul>
+    </NavBar>
     <Component {...pageProps} />
   </App>
 }
