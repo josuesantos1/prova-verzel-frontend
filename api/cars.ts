609a84f208022491b28daf058d5dc79a4128e110
josuesantos1 <josuecsantos2002@gmail.com>
2022-03-15 14:26:45 -0300

    (api:cars) One function; note: get one car

--- api/cars.ts
+++ api/cars.ts
@@ -3,3 +3,7 @@ import axios from "axios"
 export const All = async () => {
     return await axios.get(process.env.base_url + 'cars/all')
 }
+
+export const One = async (id: string | undefined) => {
+    return await axios.get(process.env.base_url + 'cars?id=' + id)
+}
